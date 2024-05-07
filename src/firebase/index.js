0eeae868fc6350f9728f37e27750fc97a6e4a6f5
Nelson Mbau <mbau.ndungu@gmail.com>
2024-05-07 10:24:03 +0300

    feat: initialize tailwind, firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+// Import SDKs for Firebase Products to be used
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+import { app } from "./init";
+
+// Initialize auth
+const auth = getAuth(app);
+
+//Initialize firestore
+const firestore = getFirestore(app);
+
+export { auth, firestore };
---
+++ src/firebase/init.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-apiX87UpiQFIpArFvxgrtbm45XQRgKk",
+  authDomain: "appointly-52ea8.firebaseapp.com",
+  projectId: "appointly-52ea8",
+  storageBucket: "appointly-52ea8.appspot.com",
+  messagingSenderId: "594020083145",
+  appId: "1:594020083145:web:7d9a434e8525f272227418",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
