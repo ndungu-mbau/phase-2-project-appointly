0658ca20077354f1766f58ed36137a73e7c3d93d
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-05-08 09:57:25 +0300

    pages/login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -23,8 +23,8 @@ export const Login = () => {
   };
 
   return (
-    <div className='max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-md'>
-       <h1 className="text-2xl font-semibold mb-4">Login</h1>
+    <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-md">
+      <h1 className="text-2xl font-semibold mb-4">Login</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username or Email:</label>
@@ -37,8 +37,16 @@ export const Login = () => {
             required
             className="mt-1 px-3 py-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
           />
+        </div>
 
-    </div>
-  )
-
-}
+        <div className="mb-4">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="mt-1 px-3 py-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50"
+          />
