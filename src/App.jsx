7ae569fa777ae6cb0480ac712ac2e88d4bc492eb
Kristian <originalkristiann@mail.com>
2024-05-08 17:06:47 +0300

    feat add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <h1 className="text-3xl font-bold">Hello World</h1>;
+  return (
+    <Navbar />
+  )
 }
 
 export default App;
