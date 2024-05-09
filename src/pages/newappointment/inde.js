c204e956bc1503ce82886d62b2f0ef5fb31a3ce2
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-05-09 13:40:33 +0300

    new appointment

--- src/pages/newappointment/inde.js
+++ src/pages/newappointment/inde.js
@@ -33,6 +33,16 @@ function newappointments () {
               <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="title">
                 Title:
               </label>
+              <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              id="title"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              required
+            />
+
 
     )
 
