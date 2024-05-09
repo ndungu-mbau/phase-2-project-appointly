4da4cc15586e3d242c972a3950ada9a688c19103
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-05-09 11:12:31 +0300

    sign up

--- src/pages/sign up/index.jsx
+++ src/pages/sign up/index.jsx
@@ -22,4 +22,28 @@ function signup () {
         // submission logic
     };
 
+    return (
+        <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h2 className="text-2xl mb-4">Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+              Username:
+            </label>
+            <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        
+
+    )
+
 }
