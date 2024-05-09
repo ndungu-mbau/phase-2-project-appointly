cbc86645d15b17efe9a2ac88f502792692d2c49f
Nelson Mbau <mbau.ndungu@gmail.com>
2024-05-09 15:04:40 +0300

    feat: add landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import { Landing } from "./landing";
 import { Login } from "./login";
-import { Auth } from "./auth"
+import { Auth } from "./auth";
 import { Dashboard } from "./dashboard";
 import { DashboardLayout } from "../layouts/dashboard-layout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
+    element: <Landing />,
   },
   {
     path: "/auth",
-    element: <Auth />
+    element: <Auth />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
   {
     path: "/dashboard",
