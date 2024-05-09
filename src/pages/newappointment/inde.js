d67a9986deadfdcb554a65ee319895a3a9cff7b1
CynanRanjiwa <NancyWanjira2894@gmail.com>
2024-05-09 13:41:03 +0300

    new appointment

--- src/pages/newappointment/inde.js
+++ src/pages/newappointment/inde.js
@@ -42,6 +42,18 @@ function newappointments () {
               onChange={handleChange}
               required
             />
+            </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="description">
+              Description:
+            </label>
+            <textarea
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="description"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              required
 
 
     )
