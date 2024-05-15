be6c2e1f28ca983c669af39a345e0ad33b9a39ad
Nelson Mbau <mbau.ndungu@gmail.com>
2024-05-15 11:48:59 +0300

    feat: add redirect logic if no user

--- src/layouts/dashboard-layout/index.jsx
+++ src/layouts/dashboard-layout/index.jsx
@@ -1,8 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
+
+import { Outlet, useNavigate } from "react-router-dom";
 import { Sidebar } from "../../components/sidebar";
 import { Navbar } from "../../components/navbar";
 
 export const DashboardLayout = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if ((!user && !loading) || error) {
+      navigate("/login");
+    }
+  }, [user, loading, error, navigate]);
   return (
     <div className="flex">
       <Sidebar />
